6a679897eba69df20a45509c2fdf0a8e365c9b51
StephenVarela <varelast92@gmail.com>
2018-09-26 15:38:58 -0400

    added header instance variable from controller to display through layout file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header="Welcome Page"
   end
 
   def about
+    @header="About Page"
+  end
+
+  def contest
+    @header="Contest Page"
   end
 
 end
